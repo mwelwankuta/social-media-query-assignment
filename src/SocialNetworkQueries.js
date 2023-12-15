1b628da990f9beec5540abe7c5e5d794f0c651b1
Mwelwa Nkuta <mwelwankuta@gmail.com>
2023-12-15 18:51:29 +0200

    promise_feature: caching

--- src/SocialNetworkQueries.js
+++ src/SocialNetworkQueries.js
@@ -1,6 +1,7 @@
 export class SocialNetworkQueries {
   constructor({ fetchCurrentUser }) {
     this.fetchCurrentUser = fetchCurrentUser;
+    this.fetchCache = [];
   }
 
   setValue(value) {
@@ -16,20 +17,49 @@ export class SocialNetworkQueries {
   }
 
   occurringBooksInFriends(friendsLikes) {
-    const likesHashMap = {}
-
-    for(const friend of friendsLikes) {
-      for (const like of friend.likes) {
-        if(likesHashMap[like] == undefined) {
-          likesHashMap[like] = 0
-          continue
+    for (const friend of friendsLikes) {
+      for (const title of friend.likes) {
+        if (this.userLikes.indexOf(title) === -1) {
+          const currentCount = this.friendLikesCount.get(title) || 0;
+          const updatedCount = currentCount + 1;
+          this.friendLikesCount.set(title, updatedCount);
         }
-        likesHashMap[like] += 1
       }
+    }
+  }
 
+  flattenLikes(friendsLikes) {
+    const likesArray = [];
+
+    for (const friend of friendsLikes) {
+      for (const like of friend.likes) {
+        likesArray.push(like);
+      }
     }
 
-    return Object.keys(likesHashMap).filter(book => likesHashMap[book] >= 1);
+    return likesArray;
+  }
+
+  calculatePotentialLikes(friends, friendsLikes, minimalScore) {
+    const potentialLikes = [];
+
+    this.friendLikesCount.forEach((count, title) => {
+      const score = count / friends.length;
+      console.log("POTENTIAL LIKE \n \n ", { score, count, title });
+      if (score >= minimalScore) {
+        potentialLikes.push(title);
+      }
+    });
+
+    console.log(
+      JSON.stringify(
+        { friends, friendsLikes, minimalScore, potentialLikes },
+        null,
+        2
+      )
+    );
+
+    return potentialLikes;
   }
 
   /**
@@ -38,49 +68,54 @@ export class SocialNetworkQueries {
    * @returns {Promise<string[]>}
    */
   async findPotentialLikes(minimalScore) {
-    const potentialLikes = [];
-
     let currentUser;
 
     try {
-      const { currentUser: user, error } = await this.initAsyncCurrentUserConstructor();
+      const { currentUser: user, error } =
+        await this.initAsyncCurrentUserConstructor();
+
       currentUser = user;
 
       if (error) {
-        throw new Error(parseErrorValue(error));
+        return [];
       }
     } catch (error) {
-      throw new Error(parseErrorValue(error));
+      return [];
     }
 
     if (!currentUser) {
+      console.log("could not fetch user");
       return [];
     }
-    
-    const { friends, likes: currentUserLikes } = currentUser
 
-    // bookInMinimumScore(minimalScore * 100 /** converting minimal score to percentage */)
+    const { friends, likes: currentUserLikes } = currentUser;
 
-    const booksUserFriendsLike = this.occurringBooksInFriends(friends)
-
-    let booksFriendsHaveRead = []
+    const userLikes = new Set(currentUserLikes);
+    const friendLikesCount = new Map();
 
     for (const friend of friends) {
-      for(const like of currentUserLikes) {
-        if(friend.likes.indexOf(like) >= 0 /** like in common */)  {
-
-          const tempBooksFriendsHaveRead = [...friend.likes?.filter(friendLike => friendLike !== like), ...booksFriendsHaveRead]
-          booksFriendsHaveRead = tempBooksFriendsHaveRead
+      for (const title of friend.likes) {
+        if (!userLikes.has(title)) {
+          friendLikesCount.set(title, (friendLikesCount.get(title) || 0) + 1);
         }
       }
     }
- 
-    console.log("Books read by most friends", booksUserFriendsLike);
-    console.log("Books friends have read that i might like ", booksFriendsHaveRead);
 
+    const potentialLikes = Array.from(friendLikesCount.entries())
+      .filter(
+        ([book, count]) =>
+          count / friends.length >= minimalScore && !userLikes.has(book)
+      )
+      .sort(([book1, count1], [book2, count2]) => {
+        if (count2 !== count1) {
+          return count2 - count1;
+        } else {
+          return book1.localeCompare(book2, "en", { sensitivity: "base" });
+        }
+      })
+      .map(([book]) => book);
 
-    // books that a said percentage of friends have liked
-    return [...booksUserFriendsLike,...booksFriendsHaveRead];
+    return potentialLikes;
   }
 }
 
